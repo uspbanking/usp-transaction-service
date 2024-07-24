97860160fd53e1208ada6c181364e1228dfebd8a
Mangeeru Anjaneyulu <Mangeeru.Anjaneyulu@BRILLIO.COM>
2024-07-24 10:04:36 +0530

    first commit

---
+++ src/main/java/com/usp/banking/UspTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.usp.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UspTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UspTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usp/banking/UspTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usp.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UspTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
